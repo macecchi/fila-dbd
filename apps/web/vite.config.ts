4fd07f26820565b3a84f67fd9837a52a77d268d3
Mario Cecchi <macecchi@gmail.com>
2026-03-06 10:11:47 -0300

    fix version display in cf

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
-const commitHash = process.env.GITHUB_SHA?.slice(0, 7) ?? 'dev';
+const commitHash = (process.env.GITHUB_SHA ?? process.env.CF_PAGES_COMMIT_SHA)?.slice(0, 7) ?? 'dev';
 
 export default defineConfig({
   base: process.env.GITHUB_ACTIONS ? '/dbd-utils/' : '/',
